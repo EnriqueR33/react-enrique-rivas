724bcdff20b9582c47d3c06600d7bb357d423acb
@EnriqueR33 <enriquerivas08@gmail.com>
2025-03-12 17:13:29 -0300

    CreaTuLanding1+Rivas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Navbar from './components/Navbar';
-import Saludo from './components/Saludo/saludo';
+import Saludo from './components/Saludo/Servicios';
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import './Navbar.css'
+import ferreteria from '../images/ferreteria.jpg';
 
 function Navbar() {
     return (
         <header>
             <nav className='nav-bar'>
                 <ul className='nav-bar-items'>
-                    <li>Inicio</li>
+                    <li>Servicios</li>
                     <li>Produtos</li>
                     <li>Contacto</li>
                 </ul>
             </nav>
+            {<div><img src="{ferreteria}" /></div>}
         </header>
     );
 };
 
-export default Navbar;
+export default Navbar; 
